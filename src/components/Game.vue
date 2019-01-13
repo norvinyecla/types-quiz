6e9e21810263ba86afcd92d3021c52a5611c7f40
Norvin Yecla <ngydev@outlook.com>
2019-01-13 20:22:08 +1100

    Fetch pokemon from API

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="game">
-    <h1>Hello, this is the Game component</h1>
-    <p>
-      {{ ct }}
-    </p>
-    <button @click="reload">
-      Reload
-    </button>
+    <h1>{{ current.name }}</h1>
+
+    <img :src="current.sprite" />
+
+    <div>
+      <button @click="reload">
+        Reload
+      </button>
+    </div>
+    <br/>
+
+    {{ current }}    
   </div>
 </template>
 
@@ -16,17 +21,59 @@ export default {
 
   data() {
     return {
-      ct: this.randomNumber() 
+      _: null,
+      ct: null,
+      info: null,
+      current: {
+        sprite: null,
+        name: null,
+        types: null
+      },
+      P: null,
+      t: null
     }
   },
 
   methods: {
-    randomNumber() {
-      return Math.floor( Math.random() * 250)
+    getRandomNumber() {
+      return Math.floor( Math.random() * 802)
     },
     reload() {
       this.ct = this.randomNumber()
+      this.fetch()
+    },
+    generateChoices(types) {
+      return types
+    },
+    fetch() {
+      var t = this
+      this.P.resource('/api/v2/pokemon/' + this.ct + '/')
+      .then(function(response) {
+        t.current.name = t._.capitalize(response.name)
+        t.current.sprite =  response.sprites.front_default, 
+
+        t.current.types = [ response.types[0].type.name ]
+        if (response.types.length > 1) {
+          t.current.types.push(response.types[1].type.name)
+        }
+
+         var choices = t.generateChoices(t.current.types)
+      }
+    )
     }
+  },
+
+  mounted() {
+    const _ = require('lodash')
+    const Pokedex = require('pokeapi-js-wrapper')
+    const P = new Pokedex.Pokedex({
+      protocol: 'https'
+    })
+    this.P = P
+    this._ = _
+    this.ct = this.getRandomNumber()
+    
+    this.fetch(this.ct)
   }
 }
 
